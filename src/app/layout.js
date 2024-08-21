70ab5c2713f0300d267a4c6ca03512f968682dac
realJimboSlice <jimm7493@stud.kea.dk>
2024-08-21 07:24:17 +0200

    Tries, yet again, to fix paths of header and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "../../components/header";
-import Footer from "../../components/Footer";
+import Header from "/components/header";
+import Footer from "/components/Footer";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
